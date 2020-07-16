73e33328d27036c6618b6580ae6f806055b0bc2b
serghalak <serghalak@gmail.com>
2020-07-16 19:44:42 +0300

    adding web data model. Closes #2

---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -0,0 +1,38 @@
+package guru.springframework.msscbeerservice.web.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+
+    private UUID id;
+    private Integer version;
+
+    private OffsetDateTime createDate;
+    private OffsetDateTime lastModifiedDate;
+
+    private String beerName;
+
+    private BeerStyleEnum beerStyle;
+
+
+    private String upc;
+
+
+    private BigDecimal price;
+
+    private Integer quantityOnHand;
+
+}
